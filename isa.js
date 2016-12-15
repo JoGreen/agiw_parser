14540cc3387a4e22f7c9475d4812e21651654e44
conrad123 <outerhaven@live.it>
2016-12-15 15:23:58 +0100

    Improved isa.js and parser.js

--- isa.js
+++ isa.js
@@ -3,7 +3,7 @@ var pos = require('pos');
 var thesaurus = require('./synonim.js');
 
 // JJ , VBG , JJ JJ JJ TO NNP -> pattern su cui ragionare (il nostro per ora taglia a JJ ,)
-let seed_regex =/(NN CC NN|NN , NN|NN , JJ NN|NN CC JJ NN|NN , CC NN|NNS CC NN|NNS CC NNS|NN CC NNS|NNS , NN|NNS , NNS|NN , NNS|NNS , JJ NN|NNS , JJ NNS|NN , JJ NNS|NNS CC JJ NN|NNS CC JJ NNS|NN CC JJ NNS|NNS , CC NN|NNS , CC NNS|NN , CC NNS)/i;
+let seed_regex =/(NN CC NN|NN , NN|NN , JJ NN|NN CC JJ NN|NN , CC NN|NNS CC NN|NNS CC NNS|NN CC NNS|NNS , NN|NNS , NNS|NN , NNS|NNS , JJ NN|NNS , JJ NNS|NN , JJ NNS|NNS CC JJ NN|NNS CC JJ NNS|NN CC JJ NNS|NNS , CC NN|NNS , CC NNS|NN , CC NNS|)/i;
 
 
 
@@ -12,12 +12,13 @@ module.exports = function(file, callback){
 	let first_sentence = data[0];
 	let tagger = new pos.Tagger();
 
-	let regex = /( is a | is an | is the | are a | are an | are the| was a | was an | was the | were a | were an | were the )/i;
+	let regex = /( is a | is an | is the | are a | are an | are the | was a | was an | was the | were a | were an | were the )/i;
 	let isa = first_sentence.split(regex); //becca solo il primo verbo essere..ottimo.
 	//console.log(isa);
 	
 	//isa[isa.length-1] = 'American linguist, philosopher, cognitive scientist, historian, social critic, and political activist.';
 	var seed = [];
+	console.log('---FRASE---\n'+first_sentence);
 	if (isa.length === 3){
 		let words = new pos.Lexer().lex(isa[isa.length-1]);
 		let taggedWords = tagger.tag(words);
@@ -26,7 +27,6 @@ module.exports = function(file, callback){
 		for (i in taggedWords){
 			tag += taggedWords[i][1]+ ' '; // postag della prima frase dopo 'is a' 
 		};
-		console.log('---FRASE---\n'+first_sentence);
 		console.log('\n---TAG---\n'+tag);
 		regex= /( VBN | VB | VBD | VBG | VBP | VBZ | IN )/i;
 		tag = tag.split(regex)[0];
@@ -38,12 +38,10 @@ module.exports = function(file, callback){
 		console.log(isa_split);
 		for (index = tag_split.length-1; index > -1;  index--){
 			//console.log(tag_split[index]+'---');
-			let bool = tag_split[index] === 'NN' ||tag_split[index] === 'NNS';
+			//let bool = tag_split[index] === 'NN' || tag_split[index] === 'NNS';
 			//console.log(bool);
-			if(tag_split[index] === 'NN' || tag[index] === 'NNS'){
-				isa_split[index] = 
-
-				isa_split[index].replace(/(\.|;|:)/,''); //se una parola è seguita da uno dei seguenti simboli senza uno spazio la puliamo per evitare che thesaurus poi si incazzi
+			if(tag_split[index] === 'NN' || tag_split[index] === 'NNS'){
+				isa_split[index] = isa_split[index].replace(/(\.|;|:)/,''); //se una parola è seguita da uno dei seguenti simboli senza uno spazio la puliamo per evitare che thesaurus poi si incazzi
 				seed.push(isa_split[index]);
 				//console.log(tag.split(seed_regex).length)
 				if(tag.split(seed_regex).length <3){
--- parser.js
+++ parser.js
@@ -23,112 +23,115 @@ function clean_text(sentence,separators) {
 
 module.exports = function(file,callback) {
 
-fs.readFile(file, function (err, data) {
+   fs.readFile(file, function (err, data) {
 
-   if (err) {
-      return console.error(err);
-   }
+      if (err) {
+         return console.error(err);
+      }
 
-   parseString(data, function (err, result) {
-         
-         if(err) {
-            return console.error(err);
-         }
-
-         var cont = 0;
-
-         //for(k=0;k<result.doc.page.length;k++) {
-            let title = result.doc.page[598].title;
-            var text = result.doc.page[598].revision[0].text[0]._;
-            var array_text = new Array();
-            var i = 0;
-            var text2 = text;
-
-            while(text2.indexOf('[[File:') != -1) {
-               var file = text2.substring(text2.indexOf('[[File:'));
-               array_text[i] = file.substr(0, file.indexOf(']]\n')+2);
-               i++;
-               text2 = text2.substring(text2.indexOf(']]\n') + 2);
-            }
-
-            var text2 = text;
-
-            while(text2.indexOf('[[Image:') != -1) {
-               var file = text2.substring(text2.indexOf('[[Image:'));
-               array_text[i] = file.substr(0, file.indexOf(']]\n')+2);
-               i++;
-               text2 = text2.substring(text2.indexOf(']]\n') + 2);
-            }
-
-            text2 = text;
-            var file = text2.substring(text2.indexOf('{{'));
-
-            if(text2.indexOf('\n}}') != -1) {
-               array_text[i] = file.substr(0, file.indexOf('\n}}')+3);
-            }
-
-            for(i = 0; i < array_text.length; i++) {
-               text = text.replace(array_text[i],'');
-            }  
+      parseString(data, function (err, result) {
             
-            var regex = ["<(.*?)>(.*?)</(.*?)>","<!--((.|\n)*?)-->","{{((.|\n)*?)}}","''{{((.|\n)*?)}}''","'''{{((.|\n)*?)}}'''"];
-            text = text.replace(new RegExp(regex.join('|'),'ig'),'');
-
-            var sentences = tokenizer.sentences(text);
-
-            var separators = ["\'\'\'\'\'","\'\'\'"];
-            var first_sentence = sentences[0];
-
-            var pe = clean_text(first_sentence,separators);
-            if(first_sentence.indexOf("'''"+pe[0]) !== -1) {
-               first_sentence = first_sentence.substring(first_sentence.indexOf("'''"+pe[0]));
+            if(err) {
+               return console.error(err);
             }
-            
-            if(pe.length == 0 && (typeof sentences[1] != 'undefined')) {
 
-               first_sentence = sentences[1];
-               pe = clean_text(first_sentence,separators);
+            var cont = 0;
+
+            for(k=456;k<460/*result.doc.page.length*/;k++) {
+
+               let title = result.doc.page[k].title;
+               var text = result.doc.page[k].revision[0].text[0]._;
+               var array_text = new Array();
+               var i = 0;
+               var text2 = text;
+
+               while(text2.indexOf('[[File:') != -1) {
+                  var file = text2.substring(text2.indexOf('[[File:'));
+                  array_text[i] = file.substr(0, file.indexOf(']]\n')+2);
+                  i++;
+                  text2 = text2.substring(text2.indexOf(']]\n') + 2);
+               }
+
+               var text2 = text;
+
+               while(text2.indexOf('[[Image:') != -1) {
+                  var file = text2.substring(text2.indexOf('[[Image:'));
+                  array_text[i] = file.substr(0, file.indexOf(']]\n')+2);
+                  i++;
+                  text2 = text2.substring(text2.indexOf(']]\n') + 2);
+               }
+
+               text2 = text;
+               var file = text2.substring(text2.indexOf('{{'));
+
+               if(text2.indexOf('\n}}') != -1) {
+                  array_text[i] = file.substr(0, file.indexOf('\n}}')+3);
+               }
+
+               for(i = 0; i < array_text.length; i++) {
+                  text = text.replace(array_text[i],'');
+               }  
                
+               var regex = ["<(.*?)>(.*?)</(.*?)>","<!--((.|\n)*?)-->","{{((.|\n)*?)}}","''{{((.|\n)*?)}}''","'''{{((.|\n)*?)}}'''"];
+               text = text.replace(new RegExp(regex.join('|'),'ig'),'');
+
+               var sentences = tokenizer.sentences(text);
+
+               var separators = ["\'\'\'\'\'","\'\'\'"];
+               var first_sentence = sentences[0];
+               var pe = [title].concat(clean_text(first_sentence,separators,title));
+
                if(first_sentence.indexOf("'''"+pe[0]) !== -1) {
                   first_sentence = first_sentence.substring(first_sentence.indexOf("'''"+pe[0]));
                }
+               
+               if(pe.length == 0 && (typeof sentences[1] != 'undefined')) {
 
-            }
+                  first_sentence = sentences[1];
+                  pe = clean_text(first_sentence,separators);
+                  
+                  if(first_sentence.indexOf("'''"+pe[0]) !== -1) {
+                     first_sentence = first_sentence.substring(first_sentence.indexOf("'''"+pe[0]));
+                  }
 
-            if(pe.length == 0) {
-               cont++;
-            }
-            
-            /*
-            se = text.split('[[');
-
-            for(i=1;i<se.length;i++) {
-               se[i-1] = se[i].substr(0, se[i].indexOf(']]'));
-            }
-
-            se.pop();
-            */
-         
-            //console.log("Articolo "+k+" stampato");
-
-            /*
-            fs.appendFile("output.txt", '{Frase: '+first_sentence+'}\r\n'+'['+pe.toString()+'] \r\n\r\n\r\n', function(err) {
-               if(err) {
-                  return console.log(err);
                }
-            });
-            */
 
-         //}
+               if(pe.length == 0) {
+                  cont++;
+               }
+               
+               /*
+               se = text.split('[[');
 
-         //console.log(cont+' miss');
+               for(i=1;i<se.length;i++) {
+                  se[i-1] = se[i].substr(0, se[i].indexOf(']]'));
+               }
 
-         first_sentence = first_sentence.replace(/\'|\[|\]|\((.*?)\)/ig,''); //toglie apici, quadre e tutto nelle tonde
-         first_sentence = first_sentence.replace("  "," "); //elimina i doppi spazi
-         first_sentence = first_sentence.replace(/\[\[[a-zA-Z0-9 ]*\|/ig,''); //fa rimanere solo gli anchor text delle secondary entities
-         sentences[0] = first_sentence;
-         callback(sentences);
+               se.pop();
+               */
+            
+               //console.log("Articolo "+k+" stampato");
 
+               /*
+               fs.appendFile("output.txt", '{Frase: '+first_sentence+'}\r\n'+'['+pe.toString()+'] \r\n\r\n\r\n', function(err) {
+                  if(err) {
+                     return console.log(err);
+                  }
+               });
+               */
+
+            //console.log(cont+' miss');
+
+            first_sentence = first_sentence.replace(/\'|\]|\((.*?)\)/ig,''); //toglie apici, quadre e tutto nelle tonde
+            first_sentence = first_sentence.replace("  "," "); //elimina i doppi spazi
+            first_sentence = first_sentence.replace(/[0-9].*(st|nd|rd|th) /ig,''); //elimina le sigle di first, second, third, ecc., perché pos si arrabbia
+            first_sentence = first_sentence.replace(/\[\[[a-zA-Z0-9#*_\-+;:.@^'"!?£$%&\/() ]*\|/gi,''); //fa rimanere solo gli anchor text delle secondary entities
+            first_sentence = first_sentence.replace(/\[/gi,''); //toglie tutte le quadre aperte rimaste
+            first_sentence = first_sentence.toLowerCase();
+            sentences[0] = first_sentence;
+            callback(sentences);
+
+         }
+      });
    });
-});
 }
