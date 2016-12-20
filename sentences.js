48dea5f934c598aff8d7fb34ad81fea45298fd7f
JoGreen <giorgiomaria@live.it>
2016-12-20 10:55:20 +0100

    sentences and first sentence elaboration

--- sentences.js
+++ sentences.js
@@ -16,8 +16,20 @@ module.exports = function(file,callback) {
 
 		for(i in docs) {
 			docs[i] = docs[i].replace(/\n/ig,'');
+			docs[i] = docs[i].replace(/\((.*?)\)/ig,'');
+			docs[i] = docs[i].replace("  "," ");
+			docs[i] = docs[i].replace(/[0-9].*(st|nd|rd|th) /ig,'');
+
+
 			let sentences = tokenizer.sentences(docs[i]);
 			docs_sentences.push({sentences: sentences});
+			let first_sentence = docs_sentences[i].sentences[0];
+		//	first_sentence = first_sentence.replace(/\((.*?)\)/ig,''); //toglie apici, quadre e tutto nelle tonde
+		//  first_sentence = first_sentence.replace("  "," "); //elimina i doppi spazi
+		//  first_sentence = first_sentence.replace(/[0-9].*(st|nd|rd|th) /ig,''); //elimina le sigle di first, second, third, ecc., perché pos si arrabbia
+		    first_sentence = first_sentence.toLowerCase();
+		    first_sentence = first_sentence.replace(/\.|"|:|;/ig,'');
+		    docs_sentences[i].sentences[0] = first_sentence;
 		}
 		callback(docs_sentences);
 	})
