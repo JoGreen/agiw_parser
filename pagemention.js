faf7a5a78e160ef4fd5c132410bd6bf0834b73c8
conrad123 <outerhaven@live.it>
2017-01-04 13:35:42 +0100

    Better regex in Pagemention

--- pagemention.js
+++ pagemention.js
@@ -39,15 +39,15 @@ module.exports = function(articolo){
 
 			if(tag === '<PE>'){
 
-				var reg = new RegExp("[^a-zA-Z]"+keywords[j]+"[^a-zA-Z]","ig");
+				var reg = new RegExp("[^a-zA-Z]"+keywords[j]+"[ ']","ig");
 
 				text[i] = text[i].replace(reg,function(match){
-					
+					console.log(match);
 					if(match=='' && text[i].indexOf(match)==0){
 						return tag+keywords[j]+tag;
 					}
 					else if(text[i].indexOf('[[')!==-1 && text[i].indexOf(keywords[j]) > text[i].indexOf('[[') && text[i].indexOf(keywords[j]) < text[i].indexOf(']]')) {
-						return match;
+						return match+'++++';
 					}
 					else
 						return match.charAt(0)+tag+keywords[j]+tag+match.charAt(match.length-1);
@@ -56,21 +56,28 @@ module.exports = function(articolo){
 			else if(tag === '<SEED>' || tag === '<SYNONYM>') {
 
 				if(text[i].indexOf('<PE>')==-1 && tag === '<SEED>' || text[i].indexOf('<PE>')==-1 && text[i].indexOf('<SEED>')==-1 && tag === '<SYNONYM>') {
-					var reg = new RegExp("The "+keywords[j]+"[^a-zA-Z]","ig");
+					let regs = [" The "+keywords[j]+"[^a-zA-Z]","^The"+keywords[j]+"[^a-zA-Z]"];
+					var reg = new RegExp(regs.join('|'),"ig");
 					text[i] = text[i].replace(reg,function(match){
-						if(text[i].indexOf('[[')!==-1 && text[i].indexOf(keywords[j]) > text[i].indexOf('[[') && text[i].indexOf(keywords[j]) < text[i].indexOf(']]')) {
-							return match;
-						}
-						else
-							return match.charAt(0)+tag+'The '+keywords[j]+tag+match.charAt(match.length-1);
+					if(match=='' && text[i].indexOf(match)==0){
+						return tag+keywords[j]+tag;
+					}
+					else if(text[i].indexOf('[[')!==-1 && text[i].indexOf(keywords[j]) > text[i].indexOf('[[') && text[i].indexOf(keywords[j]) < text[i].indexOf(']]')) {
+						return match;
+					}
+					else
+						return match.charAt(0)+tag+'The '+keywords[j]+tag+match.charAt(match.length-1);
 					});
 				}
 			}
 			else {
-
-				var reg = new RegExp("[.]{1} "+keywords[j]+"[^a-zA-Z]","ig");
+				let regs = ["[.]{1} "+keywords[j]+"[^a-zA-Z]","[,]{1} "+keywords[j]+"[^a-zA-Z]","^"+keywords[j]+"[^a-zA-Z]"];
+				var reg = new RegExp(regs.join('|'),"ig");
 				text[i] = text[i].replace(reg,function(match){
-					if(text[i].indexOf('[[')!==-1 && text[i].indexOf(keywords[j]) > text[i].indexOf('[[') && text[i].indexOf(keywords[j]) < text[i].indexOf(']]')) {
+					if(match=='' && text[i].indexOf(match)==0){
+						return tag+keywords[j]+tag;
+					}
+					else if(text[i].indexOf('[[')!==-1 && text[i].indexOf(keywords[j]) > text[i].indexOf('[[') && text[i].indexOf(keywords[j]) < text[i].indexOf(']]')) {
 						return match;
 					}
 					else
@@ -83,8 +90,9 @@ module.exports = function(articolo){
 		let count_pe = (text[i].match(/<PE>/g) || []).length /2;
 		let count_seeds = (text[i].match(/<SEED>/g) || []).length /2;
 		let count_syn = (text[i].match(/<SYNONYM>/g) || []).length /2;
+		let count_pron = (text[i].match(/<PRONOUN>/g) || []).length /2;
 
-		if((count_pe+count_seeds+count_syn)>0){
+		if((count_pe+count_seeds+count_syn+count_pron)>0){
 			texto.push(text[i]);
 		}
 
@@ -94,10 +102,11 @@ module.exports = function(articolo){
 	let count_pe = (testo.match(/<PE>/g) || []).length /2;
 	let count_seeds = (testo.match(/<SEED>/g) || []).length /2;
 	let count_syn = (testo.match(/<SYNONYM>/g) || []).length /2;
+	let count_pron = (testo.match(/<PRONOUN>/g) || []).length /2;
 
 	articolo.text = texto;
 
-	let articolo_obj = {id: articolo.id, keywords: keywords, primary_entity: count_pe, seeds: count_seeds, syn: count_syn};
+	let articolo_obj = {id: articolo.id, keywords: keywords, primary_entity: count_pe, seeds: count_seeds, syn: count_syn, pronoun: count_pron};
 
 	return articolo_obj;
 
